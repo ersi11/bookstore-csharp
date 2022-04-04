c16bb0a2dc65eb8d52a44834186efe80b76b31d8
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-04-04 16:41:19 +0200

    fine esercizio

--- bookstore-csharp/Program.cs
+++ bookstore-csharp/Program.cs
@@ -1,4 +1,40 @@
 ﻿
 // Esercizio BookStore C#
 
-Console.WriteLine("Inizio esercizio ")
+Console.WriteLine("Informazioni riguardo al libro: Clean Code");
+
+// Dichiarazioni variabili
+
+string titolo = "Clean Code";
+string autore = "Robert C. Martin";
+
+long ISBN = 9780132350884;
+short numeroPagine = 431;
+short pesoGrammiLibro = 660;
+double lunghezzaLibro = 17.91;
+double larghezzaLibro = 2.54;
+float profonditaLibro = 23.5f;
+float valutazioneMedia = 4.7f;
+int numeroRecensioni = 3308;
+string versioneKindle = "SI";
+string versioneCopertinaFlessibile = "SI";
+
+
+
+
+
+
+Console.WriteLine("Titolo :  " + titolo 
+    + "\nNome Autore :  " + autore 
+    + "\nISBN : " + ISBN 
+    + "\nNumero Pagine :  " + numeroPagine 
+    + "\nPeso Libro : " + pesoGrammiLibro 
+    + "\nLunghezza Libro :  " + lunghezzaLibro 
+    + "\nAltezza Libro :  " + larghezzaLibro 
++ "\nProfondità Libro :  " + profonditaLibro 
++ "\nValutazione Media :  " + valutazioneMedia 
++ "\nRecensioni Libro :  " + numeroRecensioni 
++ "\nVersione Kidle :  " + versioneKindle 
++ "\nCopertina Flessibile :  " + versioneCopertinaFlessibile);
+
+
